d3125382d15ac2b160e53304f36db1d9bc4dc6b3
Ben Torn <tornben@gmail.com>
2020-12-07 17:44:58 -0800

    Uploaded day 6 results

--- day6.js
+++ day6.js
@@ -1,36 +1,7 @@
 const { group } = require('console');
 const fs = require('fs');
-const data = fs.readFileSync('day6-test.txt').toString();
+const data = fs.readFileSync('day6.txt').toString();
 const input = data.split('\r\n');
-
-let uniqueAnswers = {};
-let questionsAllAnswered = [];
-let peopleInGroup = 0;
-
-input.forEach(function(answer, i) {
-    for (var i = 0; i < answer.length; i++) {
-        if (uniqueAnswers.hasOwnProperty(answer[i])) {
-            uniqueAnswers[answer[i]]++;
-        } else {
-            uniqueAnswers[answer[i]] = 1;
-        }
-    } 
-
-    peopleInGroup++;
-    
-    if (answer === '') {
-        uniqueAnswers = {}
-        peopleInGroup = 0;
-    }
-    // console.log(uniqueAnswers);
-    // console.log(peopleInGroup);
-    for (const [key, value] of Object.entries(uniqueAnswers)) {
-        console.log(key, value);
-    }
-    console.log("------")
-})
-
-
 
 function partOne(input) {
     // Groups each group's answers into single string in groupAnswers variable
@@ -70,4 +41,39 @@ function partOne(input) {
     return sumOfAnswers;
 }
 
-// console.log("Part one answer: "+ partOne(input));
+function partTwo(input) {
+    let uniqueAnswers = {people: 1};
+    let questionsAllAnswered = [];
+    let finalCount = 0;
+    
+    input.forEach(function(answer, i) {
+        for (var j = 0; j < answer.length; j++) {
+            if (uniqueAnswers.hasOwnProperty(answer[j])) {
+                uniqueAnswers[answer[j]]++;
+            } else {
+                uniqueAnswers[answer[j]] = 1;
+            }
+        } 
+      
+        if (input[i + 1] === '' || input[i + 1] === undefined) {
+            questionsAllAnswered.push(uniqueAnswers)
+            uniqueAnswers = {people: 0}
+        } else {
+            uniqueAnswers.people++;
+        }
+    })
+    
+    questionsAllAnswered.forEach(function (group) {
+        for (const [key, value] of Object.entries(group)) {
+            if (key !== "people" && value !== group.people) {
+                delete group[key];
+            } else if (key !== "people") {
+                finalCount++;
+            }
+        }
+    })
+    return finalCount
+}
+
+console.log("Part one answer: "+ partOne(input));
+console.log("Part two answer: "+ partTwo(input));
