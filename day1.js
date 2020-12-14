49dbd6c3921b5dc53e4efe8758575876cbdf302f
Ben Torn <tornben@gmail.com>
2020-12-13 22:32:46 -0800

    Refactored day1 code to be more efficient
    Changed efficiency fron big O(n^2) to O(nlogn)

--- day1.js
+++ day1.js
@@ -2,14 +2,32 @@ const fs = require('fs');
 const data = fs.readFileSync('day1.txt').toString();
 const input = data.split('\r\n').map(Number);
 
-function sumto2020(input) {
-  for (var i = 0; i < input.length; i++) {
-    for (var j = 1; j < input.length; j++) {
-      for (var k = 2; k < input.length; k++) {
-        if ((input[i] + input[j] + input[k]) === 2020) {
-          return input[i] * input [j] * input [k];
-        }
-      }
+input.sort((a, b) => a - b);
+
+const findTarget = (target, l = 0, r = input.length - 1) => {
+    const num1 = input[l];
+    const num2 = input[r];
+    const sum = num1 + num2;
+
+    if (l >= r) {
+        return -1;
+    } else if (sum === target) {
+        return num1 * num2;
+    } else if (sum < target) {
+        return findTarget(target, l + 1, r);
+    } else if (sum > target) {
+        return findTarget(target, l, r - 1);
     }
-  }
-}
+};
+
+let result = 1;
+input.forEach(function (num) {
+    const product = findTarget(2020 - num);
+
+    if (product > -1) {
+        result = result * num;
+    }
+})
+
+console.log("Part one answer: " + findTarget(2020));
+console.log("Part two answer: " + result)
